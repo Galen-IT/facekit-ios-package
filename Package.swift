e5b43b3ba568571719e40993dcbcde360c9f4090
Ivan O <ioparin@pm.me>
2022-03-30 03:26:09 +0400

    finalizing package manifest

--- Package.swift
+++ Package.swift
@@ -3,26 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "galenit-binah",
+    name: "galenit-facekit",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         .library(
-            name: "GalenitBinah",
-            targets: ["GalenitBinah"]
+            name: "GalenitFaceKit",
+            targets: ["GalenitFaceKit"]
         ),
     ],
     targets: [
-        .target(
-            name: "GalenitBinah",
-            dependencies: ["BinahAI"],
-            path: "Sources"
-        ),
         .binaryTarget(
-            name: "BinahAI",
-            url: "https://github.com/galen-it/binah-ios-package/releases/download/v1.0.0/BinahAI.xcframework.zip",
-            checksum: "c222ad6682912c3abed6ae56ba6dd6916dc45342aaed8c852670517ff0e477d3"
+            name: "GalenitFaceKit",
+            url: "https://github.com/galen-it/facekit-ios-package/releases/download/v1.0.0/GalenitFaceKit.xcframework.zip",
+            checksum: "c77f0590251c8bbf31a0b0ede67a37c851eb2a7c6c0340c2282ad0842a675195"
         )
     ]
 )
