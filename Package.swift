506e1c082dcc03a5d6e454dd6597718958b27295
Ivan O <ioparin@pm.me>
2022-03-29 22:19:03 +0400

    package manifest finalization

--- Package.swift
+++ Package.swift
@@ -1,10 +1,9 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "GalenitBinah",
+    name: "binah-ios-package",
     platforms: [
         .iOS(.v14)
     ],
@@ -22,7 +21,7 @@ let package = Package(
         ),
         .binaryTarget(
             name: "BinahAI",
-            url: "https://github.com/Galen-IT/FlowVit_PublicSDKs_iOS/releases/download/v1.0.0/BinahAI.xcframework.zip",
+            url: "https://github.com/galen-it/binah-ios-package/releases/download/v1.0.0/BinahAI.xcframework.zip",
             checksum: "c222ad6682912c3abed6ae56ba6dd6916dc45342aaed8c852670517ff0e477d3"
         )
     ]
