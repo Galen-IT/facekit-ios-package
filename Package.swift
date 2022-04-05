77c54b16c54a0f255cf4d5daf4a0ac7843e43d40
iv-ann <98310998+iv-ann@users.noreply.github.com>
2022-04-05 14:57:46 +0400

    updating binary artefact url

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GalenitFaceKit",
-            url: "https://github.com/galen-it/facekit-ios-package/releases/download/1.0.0/GalenitFaceKit.xcframework.zip",
+            url: "https://github.com/galen-it/facekit-ios-package/releases/download/1.0.1/GalenitFaceKit.xcframework.zip",
             checksum: "076acb5a61705f7f36aca42b73da008359ed40cab1433ce018ed40af2c13124b"
         )
     ]
